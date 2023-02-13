9ac2603cb60e02506254009600583f38c1695f54
dipanwita <cdipanwita04@gmail.com>
2023-02-13 13:39:17 +0530

    first commit

--- src/test/java/com/mohs10/scripts/HomeTest.java
+++ src/test/java/com/mohs10/scripts/HomeTest.java
@@ -18,69 +18,72 @@ public class HomeTest extends StartBrowser {
 		
 	
 	
-	/*
-	 * @Test public void login() throws Exception { WebDriver driver =
-	 * StartBrowser.beforeClass("firefox", "https://ecommerce.mohs10.io/store/");
-	 * CommonFuns coms = new CommonFuns();
-	 * 
-	 * coms.zipcode("82701a");
-	 * 
-	 * Thread.sleep(5000); driver.close(); }
-	 */
-	 
-	/*
-	 * @Test public void login() throws Exception { WebDriver
-	 * driver=StartBrowser.beforeClass("firefox",
-	 * "https://register.rediff.com/register/register.php?FormName=user_details");
-	 * CommonFuns coms= new CommonFuns();
-	 * 
-	 * coms.password("04@g#Q*");
-	 * 
-	 * 
-	 * Thread.sleep(5000); driver.close();}
-	 */
 	
-	
-	
-	/*
-	 * @Test public void age() throws Exception { WebDriver driver =
-	 * StartBrowser.beforeClass("firefox",
-	 * "https://demo.guru99.com/test/newtours/register.php"); CommonFuns coms = new
-	 * CommonFuns();
-	 * 
-	 * coms.age("10");
-	 * 
-	 * Thread.sleep(5000); driver.close(); }
-	 */
+	  @Test public void login() throws Exception { WebDriver driver =
+	  StartBrowser.beforeClass("firefox", "https://ecommerce.mohs10.io/store/");
+	  CommonFuns coms = new CommonFuns();
+	  
+	  coms.zipcode("827017");
+	  
+	  Thread.sleep(5000); driver.close(); }
 	 
 	 
 	
-	/*
-	 * @Test public void Userid() throws Exception {
-	 * 
-	 * WebDriver driver =
-	 * StartBrowser.beforeClass("firefox","https://wordpress.com/log-in/");
-	 * CommonFuns coms = new CommonFuns(); coms.Userid("ta44"); Thread.sleep(5000);
-	 * driver.close();
-	 * 
-	 * }
-	 */
+	  @Test public void login2() throws Exception { WebDriver
+	  driver=StartBrowser.beforeClass("firefox",
+	  "https://register.rediff.com/register/register.php?FormName=user_details");
+	  CommonFuns coms= new CommonFuns();
+	  
+	  coms.password("04@g#Q*");
+	  
+	  
+	  Thread.sleep(5000); driver.close();}
 	 
-	 /* @Test public void age() throws Exception { WebDriver driver =
+	
+	
+	
+	
+	  @Test public void age() throws Exception { WebDriver driver =
+	  StartBrowser.beforeClass("firefox",
+	  "https://demo.guru99.com/test/newtours/register.php"); CommonFuns coms = new
+	  CommonFuns();
+	  
+	  coms.age("10");
+	  
+	  Thread.sleep(5000); driver.close(); }
+	 
+	 
+	 
+	
+	
+	
+	  @Test public void Userid() throws Exception {
+	  
+	  WebDriver driver =
+	  StartBrowser.beforeClass("firefox","https://wordpress.com/log-in/");
+	  CommonFuns coms = new CommonFuns(); coms.Userid("ta44fffg@99");
+	  Thread.sleep(5000); driver.close();
+	  
+	  }
+	 
+	 
+	 
+	  @Test
+	  public void age8() throws Exception { WebDriver driver =
 			  StartBrowser.beforeClass("firefox","https://ecommerce.mohs10.io/store/"); CommonFuns coms = new
 			  CommonFuns();
 			  
 			  coms.addTocart();
 			  
 			  Thread.sleep(5000); driver.close(); }
-}*/
-		@Test
-		public void Userid() throws Exception {
 
-			WebDriver driver = StartBrowser.beforeClass("firefox", "https://wordpress.com/log-in/");
-			CommonFuns coms = new CommonFuns();
-			coms.Emailvalidate("ta44fh");
-			Thread.sleep(5000);
-			driver.close();
 	  
-} }
+			/*
+			 * @Test public void Userid5() throws Exception {
+			 * 
+			 * WebDriver driver = StartBrowser.beforeClass("firefox",
+			 * "https://wordpress.com/log-in/"); CommonFuns coms = new CommonFuns();
+			 * coms.Emailvalidate("ta44fh"); Thread.sleep(5000); driver.close();
+			 */
+	  
+} 
